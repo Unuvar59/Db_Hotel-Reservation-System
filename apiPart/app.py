f0645e2c14484bd1d9f86817c25609ea7e18660f
Furkan Ünüvar <77577812+Unuvar59@users.noreply.github.com>
2024-12-26 08:47:50 +0300

    connect new routes

--- apiPart/app.py
+++ apiPart/app.py
@@ -3,6 +3,13 @@ from flask_jwt_extended import JWTManager
 from routes.customers import customers_bp
 from routes.reservations import reservations_bp
 from routes.payments import payments_bp
+from routes.customerevents import customerevents_bp
+from routes.employees import employees_bp
+from routes.events import events_bp
+from routes.feedback import feedback_bp
+from routes.rooms import rooms_bp
+from routes.roomservices import roomservices_bp
+
 from utils.auth import auth_bp
 
 app = Flask(__name__)
@@ -14,6 +21,12 @@ app.register_blueprint(auth_bp, url_prefix='/api/auth')
 app.register_blueprint(customers_bp, url_prefix='/api/customers')
 app.register_blueprint(reservations_bp, url_prefix='/api/reservations')
 app.register_blueprint(payments_bp, url_prefix='/api/payments')
+app.register_blueprint(customerevents_bp, url_prefix='/api/customerevents')
+app.register_blueprint(employees_bp, url_prefix='/api/employees')
+app.register_blueprint(events_bp, url_prefix='/api/events')
+app.register_blueprint(feedback_bp, url_prefix='/api/feedback')
+app.register_blueprint(rooms_bp, url_prefix='/api/rooms')
+app.register_blueprint(roomservices_bp, url_prefix='/api/roomservices')
 
 if __name__ == '__main__':
     app.run(debug=True)
